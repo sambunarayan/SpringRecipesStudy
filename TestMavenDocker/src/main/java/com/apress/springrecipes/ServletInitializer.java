5804d6f087009e0a7688cc6058be71c9c2c5bab3
sambunarayan <soyou30257@gmail.com>
2020-09-19 17:23:12 +0900

    20200919 Docker Test with Maven

---
+++ TestMavenDocker/src/main/java/com/apress/springrecipes/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.apress.springrecipes;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestMavenDockerApplication.class);
+	}
+
+}
---
+++ TestMavenDocker/src/test/java/com/apress/springrecipes/TestMavenDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apress.springrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMavenDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
